11c3f8dca37d6d8b4510027eb876eea00f8b1a7d
remembermewhy@gmail.com <remembermewhy@gmail.com@a9b16d51-240e-339d-193f-7e7220e4cb0a>
2013-02-23 16:03:41 +0000

    2 modules

---
+++ branches/module4gui/gui/src/main/java/org/rememberme/gui/Main.java
@@ -0,0 +1,13 @@
+package org.rememberme.gui;
+
+/**
+ *
+ * @author remembermewhy
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    
+}
